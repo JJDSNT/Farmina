b13d849f87780194adbb797e142522b033aeba8b
Jaime Dias <75969620+JJDSNT@users.noreply.github.com>
2025-05-23 17:39:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const useMockCheckbox = document.getElementById("use-mock");
   const produtosContainer = document.getElementById("produtos");
 
-  document.getElementById('form-simulador').addEventListener('submit', function (e) {
+  document.getElementById('form-simulador').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const useMock = useMockCheckbox?.checked;
@@ -12,7 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
     if (useMock) {
       renderProdutos(MOCK_PRODUCTS);
     } else {
-      fetch("/api/proxy")
+      const form = document.getElementById("form-simulador");
+      const formData = new FormData(form);
+      const params = new URLSearchParams();
+
+      for (const [key, value] of formData.entries()) {
+        if (key === "specialcares") {
+          params.append(key, value); // múltiplos valores permitidos
+        } else {
+          params.set(key, value);
+        }
+      }
+
+      fetch("/api/proxy?" + params.toString())
         .then(res => {
           if (!res.ok) throw new Error("Erro na resposta da API");
           return res.json();
@@ -45,17 +57,15 @@ document.addEventListener("DOMContentLoaded", function () {
     produtosContainer.style.display = "block";
     produtosContainer.innerHTML = '<h3>Produtos Recomendados</h3>';
     produtos.forEach(p => {
-
       produtosContainer.innerHTML += `
-  <div class="produto">
-    <img src="${p.img_thumbnail}" width="100">
-    <div class="produto-info">
-      <strong>${p.name}</strong>
-      <span>${p.description}</span>
-      <a href="${p.url}" target="_blank">Ver Produto</a>
-    </div>
-  </div>`;
-
+        <div class="produto">
+          <img src="${p.img_thumbnail}" width="100">
+          <div class="produto-info">
+            <strong>${p.name}</strong>
+            <span>${p.description}</span>
+            <a href="${p.url}" target="_blank">Ver Produto</a>
+          </div>
+        </div>`;
     });
   }
 });
